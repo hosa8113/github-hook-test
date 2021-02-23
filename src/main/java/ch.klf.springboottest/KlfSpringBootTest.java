8488d835dcf1899a42d4e90c65d73246bbd99a66
sam <samuel.hofmann@klf.ch>
2021-02-23 22:35:02 +0100

    Dockerfile update

--- src/main/java/ch.klf.springboottest/KlfSpringBootTest.java
+++ src/main/java/ch.klf.springboottest/KlfSpringBootTest.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class KlfSpringBootTest {
 
     public static void main(String[] args) {
+        System.out.println("Here we go!");
         SpringApplication.run(KlfSpringBootTest.class, args);
     }
 }
